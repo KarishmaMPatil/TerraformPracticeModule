3780ba5a6fa3f629ad396d0668cc9c338bf0b705
KarishmaMPatil <karipatil16@gmail.com>
2026-02-11 10:23:07 +0000

    tfstatelockfile

---
+++ statelocktf/variable.tf
@@ -0,0 +1,29 @@
+variable "instanceami" {
+    default =   "ami-019715e0d74f695be"
+} 
+
+variable "instancetype" {
+   default = "t3.micro"
+}
+
+variable "keyname" {
+   default = "ubuntu"
+}
+
+variable "sg" {
+    default = "sg-0d77ee32ad3dde7d4"
+} 
+variable "api_termination" {
+  default = false
+} 
+
+variable "nosofinstances" {
+  default = 2
+}
+
+
+#data types 
+#  1    =   number 
+#  "anup", "1" = string 
+#  true  = boolean 
+#  2.1 = float  
