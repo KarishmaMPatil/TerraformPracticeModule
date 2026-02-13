1ae7d9d373ebdce22eaf94e085b762d4d2590ce2
KarishmaMPatil <karipatil16@gmail.com>
2026-02-13 11:54:44 +0000

    provision

--- provisionerBlock/privisioner.tf
+++ provisionerBlock/privisioner.tf
@@ -6,6 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "webserver2" {
     ami = "ami-0c1fe732b5494dc14"     #amzon linux
+    vpc_security_group_ids = ["sg-09ae24b53482e0b20"]
     instance_type = "t3.micro"
     key_name = "tf"
     
