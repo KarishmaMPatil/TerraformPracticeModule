877cc762fd1a07c98a0db7d39b050b89b9f9f8fe
KarishmaMPatil <karipatil16@gmail.com>
2026-02-15 10:23:57 +0000

    user

--- IAMuser/resource.tf
+++ IAMuser/resource.tf
@@ -7,3 +7,8 @@ resource "aws_iam_user_login_profile" "example" {
   user            = aws_iam_user.example_user.name
   password_length = 10
 }
+
+output "iam_user_password" {
+  value     = aws_iam_user_login_profile.example.password
+  sensitive = true
+}
