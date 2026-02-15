4524016da5a1a66cc242bbdf9de2444c4c24b29b
KarishmaMPatil <karipatil16@gmail.com>
2026-02-15 15:37:31 +0000

    user

--- VPCPeering/main.tf
+++ VPCPeering/main.tf
@@ -15,11 +15,13 @@ resource "aws_vpc" "vpc2" {
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.vpc1.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet2" {
   vpc_id     = aws_vpc.vpc2.id
   cidr_block = "192.168.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 
