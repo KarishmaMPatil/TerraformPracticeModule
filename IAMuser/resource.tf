047feaaacaeded5dfb628726fdcde2de931b3114
KarishmaMPatil <karipatil16@gmail.com>
2026-02-15 10:21:17 +0000

    user

--- IAMuser/resource.tf
+++ IAMuser/resource.tf
@@ -4,7 +4,6 @@ resource "aws_iam_user" "example_user" {
 
 
 resource "aws_iam_user_login_profile" "example" {
-  user                    = aws_iam_user.example_user.name
-  password                = "Temp@12345"
-  password_reset_required = true
+  user            = aws_iam_user.example_user.name
+  password_length = 10
 }
