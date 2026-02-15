a25f9c1f54ef33b454d0fef88e7fe6b71ed278c5
KarishmaMPatil <karipatil16@gmail.com>
2026-02-15 15:05:40 +0000

    user

--- VPCPeering/main.tf
+++ VPCPeering/main.tf
@@ -80,7 +80,7 @@ resource "aws_route" "route2" {
 
 resource "aws_instance" "ec2_vpc1" {
   ami           = "ami-0c1fe732b5494dc14"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sgiu_vpc1.id]
 
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_vpc1" {
 
 resource "aws_instance" "ec2_vpc2" {
   ami           = "ami-0c1fe732b5494dc14"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.subnet2.id
   vpc_security_group_ids = [aws_security_group.sgiu_vpc2.id]
 
