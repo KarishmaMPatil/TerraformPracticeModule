b88e663044beb50413491f02159c5b27d295d6ac
KarishmaMPatil <karipatil16@gmail.com>
2026-02-15 12:26:58 +0000

    user

---
+++ STHREEBucket/main.tf
@@ -0,0 +1,51 @@
+
+
+# Create S3 Bucket
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
+
+# Enable static website hosting
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+# Make bucket public
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Bucket Policy
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+    }]
+  })
+}
+
+# Upload index.html file
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
